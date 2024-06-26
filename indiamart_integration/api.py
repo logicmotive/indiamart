3905794e4f7e56afda0df7f806cda9f9746cf8b5
logicmotive <167954485+logicmotive@users.noreply.github.com>
2024-06-26 09:57:49 +0530

    Update api.py

--- indiamart_integration/api.py
+++ indiamart_integration/api.py
@@ -1,14 +1,8 @@
-
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import cint, format_datetime, add_days, today, date_diff, getdate, get_last_day, flt, nowdate
 from frappe import throw, msgprint, _
 from datetime import date
-import re
 import json
-import traceback
-import urllib
-from urllib.request import urlopen
 import requests
 
 @frappe.whitelist()
@@ -32,37 +26,39 @@ def sync_india_mart_lead(from_date, to_date):
                 msg=_('URL, Mobile, Key mandatory for Indiamart API Call. Please set them and try again.'),
                 title=_('Missing Setting Fields')
             )
-
         req = get_request_url(india_mart_setting, from_date, to_date)
         res = requests.post(url=req)
         
         if res.text:
-            frappe.msgprint(_("URL Response: {0}").format(res.text), raise_exception=True)
+            frappe.msgprint(_("URL Response: {0}").format(res.text))
             count = 0
             for row in json.loads(res.text):
-                if not row.get("Error_Message") is None:
+                if "Error_Message" in row:
                     frappe.throw(row["Error_Message"])
                 else:
                     doc = add_lead(row)
                     if doc:
                         count += 1
             if count > 0:
-                frappe.msgprint(_("{0} Lead Created").format(count))
-
+                frappe.msgprint(_("{0} Lead(s) Created").format(count))
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), _("India Mart Sync Error"))
+        raise
 
 def get_request_url(india_mart_setting, from_date, to_date):
-    req = str(india_mart_setting.url)
-    # +'glusr_crm_key='+str(india_mart_setting.key)+'&start_time='+str(india_mart_setting.from_date)+'&end_time='+str(india_mart_setting.to_date)
-    return req
+    return (f"{india_mart_setting.url}?"
+            f"glusr_crm_key={india_mart_setting.key}&"
+            f"start_time={from_date}&"
+            f"end_time={to_date}")
 
 @frappe.whitelist()
 def cron_sync_lead():
     try:
-        sync_india_mart_lead(today(), today())
+        today = frappe.utils.today()
+        sync_india_mart_lead(today, today)
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), _("India Mart Sync Error"))
+        raise
 
 @frappe.whitelist()
 def add_lead(lead_data):
@@ -71,12 +67,13 @@ def add_lead(lead_data):
             doc = frappe.get_doc({
                 "doctype": "Lead",
                 "first_name": lead_data["SENDER_NAME"],
-                "email_id": lead_data["SENDER_EMAI"],
+                "email_id": lead_data.get("SENDER_EMAIL", ""),
                 "mobile_no": lead_data["SENDER_MOBILE"],
-                # "requirement": lead_data["SUBJECT"],
                 "job_title": lead_data["UNIQUE_QUERY_ID"],
-                "source": "India Mart"
+                "source": "India Mart",
+                "custom_indiamart_id": lead_data["QUERY_ID"]
             }).insert(ignore_permissions=True)
             return doc
     except Exception as e:
-        frappe.log_error(frappe.get_traceback())
+        frappe.log_error(frappe.get_traceback(), _("Error adding lead from India Mart"))
+        raise
